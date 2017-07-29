737ef5e8a24f5c9588ed2c6de2c2efe1be2c96c6
Zayne Berndt <berndt.zayne@gmail.com>
2017-07-29 16:36:12 +0800

    bootstrap firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,22 @@
 (function() {
+    function config($locationProvider, $stateProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+              });
+ 
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
+     }
+    
+    
+    
      angular
-         .module('messenger', ['ui.router'])
+         .module('messenger', ['ui.router', 'firebase'])
+         .config(config); 
  })();
